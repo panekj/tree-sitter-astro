c3398e8a4eeae04635fc7402d5a708f48633f98d
Jakub Panek <me@panekj.dev>
2024-05-07 14:26:42 +0200

    fix: tree-sitter generate

---
+++ bindings/c/tree-sitter-astro.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_ASTRO_H_
+#define TREE_SITTER_ASTRO_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_astro(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_ASTRO_H_
